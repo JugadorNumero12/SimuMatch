9da0c87eec9bb3c73cec20e00682ceb61d1de364
Daniel Escoz <darkhogg@gmail.com>
2012-11-14 22:18:44 +0100

    Added method `get` to `Action`

---
+++ src/dataManager/Action.java
@@ -0,0 +1,22 @@
+package dataManager;
+
+/**
+ * An action that can be performed by a team in a match.
+ */
+public enum Action {
+	CALDEAR_AMBIENTE, OLA, PANCARTA, PITAR, SOBORNAR, APOSTAR, DOBLAR_APUESTA, EVENTO_BENEFICO, ENTREVISTA;
+	
+	/**
+	 * @param name
+	 *            The name of the action to retrieve
+	 * @return The <tt>Action</tt> associated with the given <tt>name</tt>, or <tt>null</tt> if there is no such
+	 *         <tt>Action</tt>
+	 */
+	public static Action get ( String name ) {
+		try {
+			return Enum.valueOf( Action.class, name );
+		} catch ( IllegalArgumentException exc ) {
+			return null;
+		}
+	}
+}
