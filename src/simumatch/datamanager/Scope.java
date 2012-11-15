0ca346bf60f7d67db3c9699c50300e5759ae7295
Daniel Escoz <darkhogg@gmail.com>
2012-11-15 12:28:30 +0100

    Re-commited `Scope` changes

--- src/simumatch/datamanager/Scope.java
+++ src/simumatch/datamanager/Scope.java
@@ -6,5 +6,19 @@ public enum Scope {
 	PEOPLE,
 	ENCOURAGE,
 	OFFENSIVE_SPIRIT,
-	DEFFENSIVE_SPIRIT
+	DEFENSIVE_SPIRIT;
+	
+	/**
+	 * @param name
+	 *            The name of the scope to retrieve
+	 * @return The <tt>Scope</tt> associated with the given <tt>name</tt>, or <tt>null</tt> if there is no such
+	 *         <tt>Scope</tt>
+	 */
+	public static Scope get ( String name ) {
+		try {
+			return Enum.valueOf( Scope.class, name );
+		} catch ( IllegalArgumentException exc ) {
+			return null;
+		}
+	}
 }
