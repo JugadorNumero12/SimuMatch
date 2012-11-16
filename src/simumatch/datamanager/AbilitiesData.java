1ffd6334baef1642aa7d7b785cac38b027e826d3
Daniel Escoz <darkhogg@gmail.com>
2012-11-16 11:05:56 +0100

    Added a missing import.

--- src/simumatch/datamanager/AbilitiesData.java
+++ src/simumatch/datamanager/AbilitiesData.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
