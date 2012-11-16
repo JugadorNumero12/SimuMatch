7471b44624498b289ef384da06c966e3b422bbb3
Daniel Escoz <darkhogg@gmail.com>
2012-11-16 14:25:50 +0100

    Made  compile

--- src/simumatch/datamanager/EffectComparator.java
+++ src/simumatch/datamanager/EffectComparator.java
@@ -4,28 +4,37 @@ import java.util.Comparator;
 
 import simumatch.common.Effect;
 import simumatch.common.Operator;
-
+import simumatch.common.Scope;
+import simumatch.common.Target;
 
 public final class EffectComparator implements Comparator<Effect> {
-
+	
 	@Override
 	public int compare ( Effect effect1, Effect effect2 ) {
 		if ( effect1 == null || effect2 == null ) {
 			throw new NullPointerException();
 		}
-	int aux = compareOperators( effect1.getOperator(), effect2.getOperator() );
-	if(aux != 0)return aux;
-	aux = compareScope(effect1.getScope(), effect2.getScope() );
-	if(aux != 0) return aux;
-	aux = compareTarget (effect1.getTarget(), effect2.getTarget() );
-	if(aux != 0) return aux;
-	aux = private static int compareBonus( effect1.getBonus() , effect2.getBonus() );
-	if( aux != 0 )	return aux;
-	return comparePermanent( effect1.isPermanent() effect2.isPermanent() );
-	
-}
+		
+		int aux = compareOperators( effect1.getOperator(), effect2.getOperator() );
+		if ( aux != 0 ) {
+			return aux;
+		}
+		aux = compareScope( effect1.getScope(), effect2.getScope() );
+		if ( aux != 0 ) {
+			return aux;
+		}
+		aux = compareTarget( effect1.getTarget(), effect2.getTarget() );
+		if ( aux != 0 ) {
+			return aux;
+		}
+		aux = compareBonus( effect1.getBonus(), effect2.getBonus() );
+		if ( aux != 0 ) {
+			return aux;
+		}
+		return comparePermanent( effect1.isPermanent(), effect2.isPermanent() );
+		
 	}
-
+	
 	private static int compareOperators ( Operator op1, Operator op2 ) {
 		if ( op1 == Operator.PRODUCT && op2 != Operator.PRODUCT ) {
 			return -1;
@@ -36,22 +45,19 @@ public final class EffectComparator implements Comparator<Effect> {
 		}
 	}
 	
-	private static int compareScope( Scope sp1, Scope sp2 ){
-		return sp1.compareTo(sp2);
-	}
-
-	private static int compareTarget( Target tg1, Target tg2 ){
-		return tg1.compareTo(tg2);
-
+	private static int compareScope ( Scope sp1, Scope sp2 ) {
+		return sp1.compareTo( sp2 );
 	}
 	
-	private static int compareBonus( Double db1, Double db2 ){
-		return db1.compareTo(db2);
-
+	private static int compareTarget ( Target tg1, Target tg2 ) {
+		return tg1.compareTo( tg2 );
 	}
-
-	private static int comparePermanent( Boolean bl1, Boolean bl2 ){
-		return bl1.compareTo(bl2);
-
+	
+	private static int compareBonus ( Double db1, Double db2 ) {
+		return db1.compareTo( db2 );
+	}
+	
+	private static int comparePermanent ( Boolean bl1, Boolean bl2 ) {
+		return bl1.compareTo( bl2 );
 	}
 }
